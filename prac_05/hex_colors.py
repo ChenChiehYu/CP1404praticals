01351bafa9cfc419851ac25b7a7b5a6281cc2fe8
Chen Chieh Yu <Chiehyu.chen@my.jcu.edu.au>
2023-08-07 16:34:00 +0800

    Add starter

---
+++ prac_05/hex_colors.py
@@ -0,0 +1,14 @@
+CODE_TO_COLOR = {"AMBER": "#ffbf00",
+                 "ANTIQUEWHITE": "#faebd7",
+                 "AQUA": "#00ffff",
+                 "BLACK": "#000000",
+                 "CAMEL": "#c19a6b",
+                 "EGGPLANT": "#614051",
+                 "EMERALD": "#50c878",
+                 "GRAY": "#bebebe",
+                 "ICEBERG": "#71a6d2",
+                 "IRIS": "#5a4fcf"
+                 }
+print(CODE_TO_COLOR)
+
+color_code = input("Enter a ")
