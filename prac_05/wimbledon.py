ebed473dcefeb7660eb7772e5dfdba97663d2dd6
Chen Chieh Yu <Chiehyu.chen@my.jcu.edu.au>
2023-08-13 16:26:01 +0800

    Add starter

--- prac_05/wimbledon.py
+++ prac_05/wimbledon.py
@@ -1,26 +1,41 @@
 def main():
     CHAMPIONS = dict()
+    COUNTRYS = dict()
     filename = "wimbledon.csv"
     with open(filename, "r", encoding="utf-8-sig") as in_file:
-        lines = in_file.readline()
-        for line in lines:
-            line=line.split("\n")
-            for text in line:
-                if text in CHAMPIONS:
-                    CHAMPIONS[text]+=1
-                else:
-                    CHAMPIONS[text]=1
-
-    print(CHAMPIONS)
+        next(in_file)
+        lines = in_file.readlines()
+        result = get_rows(lines, 1, 2)
+        count_result(CHAMPIONS, COUNTRYS, result)
+    print_result(CHAMPIONS, COUNTRYS)
 
 
+def count_result(CHAMPIONS, COUNTRYS, result):
+    for word in result:
+        if len(word) > 3:
+            if word in CHAMPIONS:
+                CHAMPIONS[word] += 1
+            else:
+                CHAMPIONS[word] = 1
+        else:
+            COUNTRYS[word] = 1
 
-def print_champions(champions):
-    for key in champions:
-        print(champions[key], champions)
 
-def sort_country(countries):
-    sorted_countries=sorted(countries)
-    return sorted_countries
+def get_rows(lines, row1, row2):
+    result = []
+    for words in lines:
+        result.append(words.split(',')[row1])
+        result.append(words.split(',')[row2])
+    return result
+
+def print_result(CHAMPIONS, COUNTRYS):
+    print("Wimbledon Champions: ")
+    for i in CHAMPIONS.items():
+        print(f"{i[0]} {i[1]}")
+    print()
+    countries = sorted(COUNTRYS.keys())
+    print(f"These {len(countries)} countries have won Wimbledon:")
+    for j in range(0, len(countries)):
+        print(countries[j], end=", ")
 
 main()
