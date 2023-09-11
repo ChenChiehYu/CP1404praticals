daa2c36eb3747c0a8c6a9a7109c865687a6a7b84
Chen Chieh Yu <Chiehyu.chen@my.jcu.edu.au>
2023-09-11 15:35:28 +0800

    Add starter

---
+++ prac_09/unreliable_car_test.py
@@ -0,0 +1,11 @@
+from prac_09.unreliable_car import UnreliableCar
+
+car = UnreliableCar("car 1", 100, 50)
+car.drive(10)
+print(car)
+car.drive(20)
+print(car)
+car.drive(30)
+print(car)
+car.drive(40)
+print(car)
