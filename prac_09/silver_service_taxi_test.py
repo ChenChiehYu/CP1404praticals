b05dd0140e99f2cfa8711501c91499004f7c1e96
Chen Chieh Yu <Chiehyu.chen@my.jcu.edu.au>
2023-09-11 16:21:48 +0800

    Add starter

--- prac_09/silver_service_taxi_test.py
+++ prac_09/silver_service_taxi_test.py
@@ -3,4 +3,4 @@ from prac_09.silver_service_taxi import SilverServiceTaxi
 silver_taxi = SilverServiceTaxi("Hummer", 200, 2)
 silver_taxi.drive(18)
 print(silver_taxi)
-print(silver_taxi.get_fare())
+print(f"${silver_taxi.get_fare():.2f}")
